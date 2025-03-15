a3aab1a3b32fec35cbda98d4339b4a7db6368f86
Edd Gachira <edwingachira@gmail.com>
2025-03-15 04:29:19 +0300

    Implemented dynamic booking storage using slices and updated ticket count after user booking

--- main.go
+++ main.go
@@ -5,18 +5,22 @@ import "fmt"
 func main() {
 	// var conferenceName string = "Go conference"
 	conferenceName := "Go conference"
-	const conferenceTickets = 50
-	var remainingTickets = 50
+	const conferenceTickets int = 50
+	var remainingTickets uint = 50
 
 	// fmt.Println("Welcome to", conferenceName, "booking application")
 	fmt.Printf("Welcome to %v booking application\n", conferenceName)
 	fmt.Printf("We have a total of %v tickets and %v are still available\n", conferenceTickets, remainingTickets)
 	fmt.Println("Get your tickets here")
 
+	// var bookings = [50]string{}
+	// var bookings []string // changed to slice
+	bookings := []string{}
+
 	var firstName string
 	var lastName string
 	var email string
-	var userTickets int
+	var userTickets uint
 
 	fmt.Println("Enter your first name: ")
 	fmt.Scan(&firstName)
@@ -30,6 +34,13 @@ func main() {
 	fmt.Println("Enter number of tickets: ")
 	fmt.Scan(&userTickets)
 
-	fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v", firstName, lastName, userTickets, email)
+	remainingTickets = remainingTickets - userTickets
+	// bookings[0] = firstName + " " + lastName // add items to an array
+	bookings = append(bookings, firstName+" "+lastName)
+
+	fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v\n", firstName, lastName, userTickets, email)
+	fmt.Printf("%v tickets remaining for %v\n", remainingTickets, conferenceName)
+
+	fmt.Printf("These are all our bookings %v\n", bookings)
 
 }
